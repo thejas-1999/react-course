63f0f72c26a0a21d27a381beb67bb6b9deac9c6d
thejas-1999 <thejask074@gmail.com>
2024-01-09 19:12:18 +0530

    add some background color to buttons and change project name

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -9,21 +9,21 @@ function App() {
       style={{ backgroundColor: color }}
     >
       <div className=" fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg  px-3 py-2 rounded-3xl">
           <button
-            className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg bg-green-600"
             onClick={() => setColor("green")}
           >
             Green
           </button>
           <button
-            className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg bg-red-600"
             onClick={() => setColor("red")}
           >
             Red
           </button>
           <button
-            className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg bg-blue-600"
             onClick={() => setColor("blue")}
           >
             Blue
