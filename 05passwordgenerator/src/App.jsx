39f3a5046519e489d5520150661c17839b163c1a
thejas-1999 <thejask074@gmail.com>
2024-01-09 19:49:21 +0530

    create a project for password generator

---
+++ 05passwordgenerator/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="bg-green-300">Hello</h1>
+    </>
+  );
+}
+
+export default App;
